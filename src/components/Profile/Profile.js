0f649568785b36135a142772bf13b78c369541aa
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-07-11 23:45:50 +0300

    change repo

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,12 @@
-import React, {Component} from "react";
+import React from "react";
 import "./Profile.css";
 
-class Profile extends Component {
 
-    render() {
-        const {username, tag, location, avatar,stats} = this.props;    
-        return (
-             
+
+function Profile(props) {
+    const {username, tag, location, avatar,stats} = props;
+
+    return (
     <div className="profile">
     <div className="description">
         <img
@@ -34,45 +34,7 @@ class Profile extends Component {
         </li>
     </ul>
     </div>
-
-        )
-    }
+    )
 }
 
 export default Profile;
-
-// function Profile(props) {
-//     const {username, tag, location, avatar,stats} = props;
-
-//     return (
-//     <div className="profile">
-//     <div className="description">
-//         <img
-//         src={avatar ? avatar : "https://cdn-icons-png.flaticon.com/512/1077/1077012.png"}
-//         alt={username}
-//         className="avatar"
-//         />
-//         <p className="name">{username}</p>
-//         <p className="tag">{tag}</p>
-//         <p className="location">{location}</p>
-//     </div>
-
-//     <ul className="stats">
-//         <li>
-//         <span className="label">Followers</span>
-//         <span className="quantity">{stats.followers}</span>
-//         </li>
-//         <li>
-//         <span className="label">Views</span>
-//         <span className="quantity">{stats.views}</span>
-//         </li>
-//         <li>
-//         <span className="label">Likes</span>
-//         <span className="quantity">{stats.likes}</span>
-//         </li>
-//     </ul>
-//     </div>
-//     )
-// }
-
-// export default Profile;
